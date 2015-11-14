e800b1c7ee19911456b49d177a7ba175d2ffd01c
Jason Taylor <jftaylor21@gmail.com>
2015-11-14 15:04:16 -0500

    Put weather station and thingspeak constants into a single header file that will need modified for each device.

---
+++ arduino/weatherStation-thingspeak/weatherStationConstants.h
@@ -0,0 +1,14 @@
+#ifndef weatherStationConstants_hpp
+#define weatherStationConstants_hpp
+
+const int HH10D_OFFSET(7732);
+const int HH10D_SENSITIVITY(402);
+
+const float ELEVATION_IN_METERS(62.7);
+
+const byte MAC_ADDRESS[] = {0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa};
+
+const unsigned long THINGSPEAK_CHANNEL(????);
+const char* THINGSPEAK_API_KEY("API_KEY");
+
+#endif
